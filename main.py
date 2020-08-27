3032b4fefb669926e06a72d3cbd5af0fe84d7f7f
Prehistoric Buddy <faturarifm@gmail.com>
2020-08-27 04:34:42 +0000

    Python Basic Operators checkpoint

--- main.py
+++ main.py
@@ -32,3 +32,46 @@ second_name = None
 print(numbers)
 print(strings)
 print("The second name on the names list is %s" % second_name)
+
+# Basic Operators
+print("====================================")
+number = 1 + 2 * 3 / 4.0
+print(number)
+
+remainder = 11 % 3
+print(remainder)
+
+squared = 5 ** 2
+cubed = 2 ** 3
+print(squared)
+print(cubed)
+
+helloworld = "hello" + " " + "world"
+print(helloworld)
+
+lotsofhellos = "hello " * 10
+print(lotsofhellos)
+
+even_numbers = [2,4,6,8]
+odd_numbers = [1,3,5,7]
+all_numbers = odd_numbers + even_numbers
+print(all_numbers)
+
+print("====================================")
+
+x = object()
+y = object()
+
+x_list = [x] * 10
+y_list = [y] * 10
+big_list = x_list + y_list
+
+print("x_list contains %d objects" % len(x_list))
+print("y_list contains %d objects" % len(y_list))
+print("big_list contains %d objects" % len(big_list))
+
+# testing code
+if x_list.count(x) == 10 and y_list.count(y) == 10:
+    print("Almost there...")
+if big_list.count(x) == 10 and big_list.count(y) == 10:
+    print("Great!")
